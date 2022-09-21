5643efa1ac38bae6fc9e5fe64024bca421c24393
Rick Flores <rmflores6@miners.utep.edu>
2022-09-21 12:28:03 +0900

    Python

---
+++ Exercise - Credit Card Roulette.py
@@ -0,0 +1,12 @@
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+from random import randrange
+
+choice = randrange(0, len(names))
+
+print(f"\n{names[choice]} is going to buy the meal today!")
+
---
+++ Exercise - Heads or Tails.py
@@ -0,0 +1,12 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+
+random = random.randrange(2)
+print(random)
+
+if random == 0:
+    print("Tails")
+else:
+    print("Heads")
