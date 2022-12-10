6d0d564da9c49eed6bbb60c69bdfe082db94c5f3
Richard Flores <rflo147@wgu.edu>
2022-12-10 15:24:07 +0900

    12/10

---
+++ Exercise - Caesar Cipher.py
@@ -0,0 +1,44 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar().
+
+# def encrypt(plain_text, shift_amount):
+#   cipher_text = ""
+#   for letter in plain_text:
+#     position = alphabet.index(letter)
+#     new_position = position + shift_amount
+#     cipher_text += alphabet[new_position]
+#   print(f"The encoded text is {cipher_text}")
+#
+# def decrypt(cipher_text, shift_amount):
+#   plain_text = ""
+#   for letter in cipher_text:
+#     position = alphabet.index(letter)
+#     new_position = position - shift_amount
+#     plain_text += alphabet[new_position]
+#   print(f"The decoded text is {plain_text}")
+#
+# if direction == "encode":
+#   encrypt(plain_text=text, shift_amount=shift)
+# elif direction == "decode":
+#   decrypt(cipher_text=text, shift_amount=shift)
+
+def caesar(plain_text, shift_amount, direction):
+  cipher_text = ""
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    if direction == "encode":
+      new_position = position + shift_amount
+    else:
+      new_position = position - shift_amount
+    cipher_text += alphabet[new_position]
+    if len(cipher_text) == len(plain_text):
+      print(f"The new message is {cipher_text}")
+
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+
+caesar(text, shift, direction)
