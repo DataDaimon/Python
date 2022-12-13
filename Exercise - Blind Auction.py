de773266aedcbedf18780b192bb19ff43bba5bda
Rick Flores <rmflores6@miners.utep.edu>
2022-12-13 12:18:28 +0900

    12/13

--- Exercise - Blind Auction.py
+++ Exercise - Blind Auction.py
@@ -4,4 +4,19 @@ from art2 import logo
 clear = lambda: os.system('cls')
 
 print(logo)
-clear()
+print("Welcome to the blind auction program!")
+
+bid_dict = {}
+game_over = True
+
+while game_over:
+    name = str(input("Please enter your name: "))
+    bid = int(input("Please enter your bid: "))
+    bid_dict[name] = bid
+
+    addl = input("Additional bids (yes or no): ")
+    clear()
+    if addl == "no":
+        max_bid = max(bid_dict, key=bid_dict.get)
+        print(f"\nThe winner is: {max_bid}")
+        game_over = False
