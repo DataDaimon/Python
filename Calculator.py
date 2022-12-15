d4380ffd37bd22e9e5441d5cc78cdc65699237eb
Rick Flores <rmflores6@miners.utep.edu>
2022-12-15 12:17:18 +0900

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -27,24 +27,30 @@ operations = {
     "/": divide
 }
 
+num1 = int(input("Enter a number: "))
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Choose an operation: ")
+num2 = int(input("Enter second a number: "))
+calc_func = operations[operation_symbol]
+answer = calc_func(num1, num2)
+print(f"{num1} {operation_symbol} {num2} = {answer}")
+continue_game = input("Continue Calculation (Y or N): ")
+
 game_over = True
 
 while game_over:
-    num1 = int(input("Enter a number: "))
-    for symbol in operations:
-        print(symbol)
-    operation_symbol = input("Choose an operation: ")
-    num2 = int(input("Enter second a number: "))
-    calc_func = operations[operation_symbol]
-    answer = calc_func(num1, num2)
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
-    continue_game = input("Continue Calculation (Y or N): ")
-    if continue_game = "n":
+    if continue_game == "n":
         game_over = False
-    for symbol in operations:
-        print(symbol)
-    operation_symbol = input("Choose an operation: ")
-    num3 = int(input("Enter a new number: "))
-    
-
-
+    else:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Choose an operation: ")
+        num3 = int(input("Enter a new number: "))
+        calc_func = operations[operation_symbol]
+        prev_answer = answer
+        answer = calc_func(answer, num3)
+        print(f"{prev_answer} {operation_symbol} {num3} = {answer}")
+        continue_game = input("Continue Calculation (Y or N): ")
+        if continue_game == "n":
+            game_over = False
