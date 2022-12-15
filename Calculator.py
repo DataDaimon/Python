6cfd5d2420f902e143d11280c09b7e98f419a9d5
Rick Flores <rmflores6@miners.utep.edu>
2022-12-15 10:17:58 +0900

    Update Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,28 @@
+# Calculator
+
+# Add
+def add(n1, n2):
+    return n1 + n2
+
+# Subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+# Multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+# Divide
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter second a number: "))
+
