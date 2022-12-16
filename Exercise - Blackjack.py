80ed8f0852b1ddff84c713bf48c32e43a81234a5
Rick Flores <rmflores6@miners.utep.edu>
2022-12-16 12:55:01 +0900

    12/16

---
+++ Exercise - Blackjack.py
@@ -0,0 +1,30 @@
+"""
+BlackJack
+Richard Flores
+12/16/2022
+"""
+
+import random
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def rand_card():
+    new_card = random.choice(cards)
+    return new_card
+
+
+p_card1 = rand_card()
+p_card2 = rand_card()
+
+d_card1 = rand_card()
+d_card2 = rand_card()
+
+print("\nWelcome to BlackJack Casino\n")
+
+print("Your hand:")
+print(f" [{p_card1}] [{p_card2}]")
+
+print("\nDealer's Hand: ")
+print(f" [{d_card1}] [{d_card2}]")
+
+
