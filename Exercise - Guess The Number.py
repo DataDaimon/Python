097e9708eb1e1466d3350217a1bc7123d884d8f7
Rick Flores <rmflores6@miners.utep.edu>
2023-01-02 13:22:08 +0900

    Update Exercise - Guess The Number.py

--- Exercise - Guess The Number.py
+++ Exercise - Guess The Number.py
@@ -12,26 +12,35 @@ lives = 0
 
 mode = input("Choose Easy or Hard mode: ")
 if mode == "easy":
-    lives = 5
-else:
     lives = 10
+else:
+    lives = 5
 
 number = random.randint(1, 100)
 print(number)
 
-guess = int(input("\nEnter a guess: "))
+game_over = False
 
-def check_guess(int_guess, int_number):
-    global lives
-    if guess > number:
-        print("Too High!")
-        return lives - 1
-    elif guess < number:
-        print("Too Low!")
-        return lives - 1
-    else:
-        print("Winner! Winner! Chicken Dinner!")
+while not game_over:
+    guess = int(input("\nEnter a guess: "))
 
-check_guess(guess, number)
-print(lives)
+    def check_guess(int_guess, int_number):
+        global lives
+        global game_over
+        if guess > number:
+            print("Too High!")
+            lives = lives - 1
+        elif guess < number:
+            print("Too Low!")
+            lives = lives - 1
+        else:
+            print("Winner! Winner! Chicken Dinner!")
+            game_over = True
+
+    check_guess(guess, number)
+    print(f"You have {lives} lives left.")
+    if lives == 0:
+        print(f"No lives left, Game Over.")
+        game_over = True
+
 
