fa24558b204ed01960ce2772759d13e528240c2d
Rick Flores <rmflores6@miners.utep.edu>
2023-02-06 13:25:22 +0900

    2/6

---
+++ Exercise - Turtle.py
@@ -0,0 +1,11 @@
+import turtle
+from turtle import Turtle, Screen
+import prettytable
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("darkgoldenrod")
+timmy.forward(100)
+
+my_screen = Screen()
+my_screen.exitonclick()
