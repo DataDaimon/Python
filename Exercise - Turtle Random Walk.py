1903137cb0c5b33f050928646573c5762de9ecd3
Richard Flores <rflo147@wgu.edu>
2023-03-15 16:45:17 +0900

    Update Exercise - Turtle Random Walk.py

--- Exercise - Turtle Random Walk.py
+++ Exercise - Turtle Random Walk.py
@@ -1,25 +1,32 @@
 # Turtle Random Walk
 # Author: Richard Flores
 # Date: 3.15.23
-
+import turtle
 from turtle import Turtle
 from turtle import Screen
 import random
 
 tim = Turtle()
+turtle.colormode(255)
 
 tim.shape("turtle")
 tim.color("blue")
-tim.width(10)
+tim.pensize(15)
 tim.speed(4)
 
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    random_color = (r, g, b)
+    return random_color
+
 angles = (90, 180, 270, 360)
-colors = ["red","blue","gray","green","yellow","orange","pink","purple"]
 
 for _ in range(0, 100):
     tim.forward(40)
-    tim.right(random.choice(angles))
-    tim.color(random.choice(colors))
+    tim.setheading(random.choice(angles))
+    tim.color(random_color())
 
 screen = Screen()
 screen.exitonclick()
