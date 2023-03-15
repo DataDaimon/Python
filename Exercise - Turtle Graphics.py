e960ac861578e69ccc9432038ff83f5a31e4e238
Richard Flores <rflo147@wgu.edu>
2023-03-15 15:07:02 +0900

    Update Exercise - Turtle Graphics.py

--- Exercise - Turtle Graphics.py
+++ Exercise - Turtle Graphics.py
@@ -4,17 +4,33 @@
 
 from turtle import Turtle
 from turtle import Screen
+import random
 
 tim = Turtle()
 
 tim.shape("turtle")
 tim.color("blue")
 
-for _ in range(25):
-    tim.forward(5)
-    tim.penup()
-    tim.forward(5)
-    tim.pendown()
+# 360 divided by number of sides
+
+sides = 3
+colors = ['blue', 'red', 'yellow', 'green', 'purple', 'pink', 'brown']
+
+def angle(sides):
+    angle = 360 / sides
+    return angle
+
+def draw_shape(sides):
+    for _ in range(0, sides):
+        tim.forward(100)
+        tim.right(angle(sides))
+
+for _ in range(sides, 10):
+    draw_shape(sides)
+    sides += 1
+    tim.color(random.choice(colors))
+
+
 
 
 
