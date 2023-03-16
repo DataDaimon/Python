e59a241217e5f34f4897609d6fe77efa52b6a93e
Richard Flores <rflo147@wgu.edu>
2023-03-16 16:15:04 +0900

    3/16

---
+++ Exercise - Hirst Painting.py
@@ -0,0 +1,25 @@
+# Hirst Painting
+# Author: Richard Flores
+# Date: 3.16.23
+
+# import colorgram
+#
+# # Extract 6 colors from an image.
+# colors = colorgram.extract('Hirst.png', 10)
+#
+# color_tuple = []
+#
+# for n in range(0, 10):
+#     first_color = colors[n]
+#     rgb = first_color.rgb
+#     r = rgb[0]
+#     g = rgb[1]
+#     b = rgb[2]
+#     color_tuple.append((r,g,b))
+
+
+color_list = [(226, 231, 236), (58, 106, 148), (224, 200, 109), (134, 84, 58), (224, 137, 60), (196, 145, 171),
+              (234, 226, 204), (225, 234, 231), (140, 178, 205), (139, 82, 105)]
+
+
+
