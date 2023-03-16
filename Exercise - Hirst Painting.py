e63f5caf7e3abcbf0c69d78ab1710dc82535366f
Richard Flores <rflo147@wgu.edu>
2023-03-16 16:54:14 +0900

    Update Exercise - Hirst Painting.py

--- Exercise - Hirst Painting.py
+++ Exercise - Hirst Painting.py
@@ -16,10 +16,35 @@
 #     g = rgb[1]
 #     b = rgb[2]
 #     color_tuple.append((r,g,b))
+import turtle
+from turtle import Turtle
+from turtle import Screen
+import random
 
+turtle.colormode(255)
 
 color_list = [(226, 231, 236), (58, 106, 148), (224, 200, 109), (134, 84, 58), (224, 137, 60), (196, 145, 171),
               (234, 226, 204), (225, 234, 231), (140, 178, 205), (139, 82, 105)]
 
+tom = Turtle()
+
+tom.hideturtle()
+tom.penup()
+tom.speed("fastest")
+tom.goto(-225, -150)
+
+for _ in range(0, 10):
+    for _ in range(0,10):
+        tom.color(random.choice(color_list))
+        tom.dot(20)
+        tom.forward(50)
+    tom.backward(50)
+    tom.setheading(90)
+    tom.forward(50)
+    tom.setheading(180)
+    tom.forward(450)
+    tom.setheading(0)
 
 
+screen = Screen()
+turtle.exitonclick()
