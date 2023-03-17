da074dbd9eb8fbc4780b78ef4cfe38671fe2d5d5
Richard Flores <rflo147@wgu.edu>
2023-03-17 15:27:13 +0900

    Create Exercise - Turtle Race.py

---
+++ Exercise - Turtle Race.py
@@ -0,0 +1,12 @@
+# Turtle Race
+# Author: Richard Flores
+# Date: 3/17/2023
+import turtle
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=500,height=400)
+
+user_bet = screen.textinput(title="Make your bet.", prompt="Pick a color: ")
+
+turtle.exitonclick()
