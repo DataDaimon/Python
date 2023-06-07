659b7b96b8419607f2b26442a23343ea063674d3
Richard <rflo147@wgu.edu>
2023-06-07 21:46:37 +0900

    Update Exercise - Snake Game.py

--- Exercise - Snake Game.py
+++ Exercise - Snake Game.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -17,11 +18,15 @@ for position in starting_positions:
     new_segment.goto(position)
     segments.append(new_segment)
 
-screen.update()
-
 game_is_on = True
 while game_is_on:
-    for seg in segments:
-        seg.forward(20)
+    screen.update()
+    time.sleep(0.1)
+
+    for seg_num in range(len(segments) -1, 0, -1):
+        new_x = segments[seg_num -1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
 
 screen.exitonclick()
